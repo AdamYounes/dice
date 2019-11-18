2ce7b1456765c5004b526ddd74a12a93215c49b8
Adam <adamyounes02@gmail.com>
2019-11-18 10:08:08 +0100

    first commit

---
+++ tärningsimulator.py
@@ -0,0 +1,14 @@
+import random
+
+print ("Welcome to my Dice roller")
+print ("Roll a dica by pressing enter.")
+start_again = "enter"
+
+while start_again == "enter":
+    print("Rolling...")
+    print(random.randint(1,6))
+    print("again?")
+
+
+
+
